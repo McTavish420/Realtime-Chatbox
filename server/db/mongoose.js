6d60a30f8209d577f3ace99467041c9da9096dc5
McTavish420 <abirmctavish@gmail.com>
2019-11-13 16:06:22 -0800

    changed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const dbURL = 'localhost:27017/Chatbox';
 
-mongoose.connect(`mongodb+srv://MacTavish:TUSAR@142442@devcluster-cybti.mongodb.net/Chatbox?retryWrites=true`, {
+mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://MacTavish:TUSAR@142442@devcluster-cybti.mongodb.net/DataEntry?retryWrites=true`, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useMongoClient: true
 }).then(() => {
     console.log(`Database is connected successfully!!!`);
 }).catch((e) => {
